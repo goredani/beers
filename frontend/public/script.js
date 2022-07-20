6cb0a862c062e154f357bd54c1485ec1037fee1c
Daniel Gore <goredani@gmail.com>
2022-07-20 14:53:53 +0200

    first commit

---
+++ frontend/public/script.js
@@ -0,0 +1,16 @@
+// console.log(beers);
+
+// console.log(beers.cards);
+
+// console.log(beers.logo);
+
+// console.log(beers.cards[5]);
+
+
+window.addEventListener('load', loadEvent);
+
+function loadEvent() {
+    const rootElement = document.getElementById('root');
+    console.log(rootElement);
+    console.log('siker');
+}
